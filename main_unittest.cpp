518d3cd3ebd6c138d16f2b09f6100a1c5d6f060b
Date Huang <tjjh89017@hotmail.com>
2017-04-30 14:50:39 +0800

    C* testing

--- main_unittest.cpp
+++ main_unittest.cpp
@@ -41,3 +41,15 @@ TEST(triangle_type_test, EdgeTesting) {
 	EXPECT_NE(INVAILD, triangle_type(200, 5, 5));
 	EXPECT_EQ(INVAILD, triangle_type(201, 5, 5));
 }
+
+TEST(triangle_type_test, PathTesting) {
+	// c0
+	EXPECT_EQ(INVAILD, triangle_type(201, 5, 5));
+	EXPECT_EQ(INVAILD, triangle_type(5, 201, 5));
+	EXPECT_EQ(INVAILD, triangle_type(5, 5, 201));
+	EXPECT_EQ(NOT_TRIANGLE, triangle_type(200, 100, 100));
+	EXPECT_EQ(EQUILATERAL, triangle_type(100, 100, 100));
+	EXPECT_EQ(ISOSCELES, triangle_type(2, 100, 100));
+	// c1
+	EXPECT_EQ(TRIANGLE, triangle_type(5, 4, 3));
+}
